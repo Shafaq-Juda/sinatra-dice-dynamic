2e00dd5c2060f3ed4c9f2ea8c53ed593f16938c4
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-08 05:37:40 +0000

    dice sides part done easily

--- app.rb
+++ app.rb
@@ -5,14 +5,16 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/:num/:size") do
+get("/dice/:num/:sides") do
   @num_dice = params.fetch("num").to_i
+  @side_dice = params.fetch("sides").to_i
 
   @rolls = []
 
   @num_dice.times do
-    die = rand(1..6)
+    die = rand(1..@side_dice)
     @rolls.push(die)
   end
+  
   erb(:flexible)
 end
